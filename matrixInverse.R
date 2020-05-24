e7d49fffa499edb93c1b27491403f66043e76116
Sakshi Tamrakar <65227341+Sakshi12-02@users.noreply.github.com>
2020-05-24 14:39:15 +0530

    Create matrixInverse.R

---
+++ matrixInverse.R
@@ -0,0 +1,51 @@
+## matrixInverse
+makeCacheMatrix <- function(x = matrix()) {
+  invrs <- NULL
+  set <- function(y) {
+    x <- y
+    invrs <- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) invrs <- inverse
+  getinverse <- function() invrs
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+cacheSolve <- function(x, ...) {
+  invrs <= x$getinverse()
+  if(!is.null(invrs)) {
+    message("getting cached data")
+    return(invrs)
+  }
+  mat <- x$get()
+  invrs <- solve(mat, ...)
+  x$setinverse(invrs)
+  invrs
+  
+ ## Return a matrix that is the inverse of 'x'
+inv <- x$getInverse()
+if (!is.null(inv)) {
+message("getting cached data")
+return(inv)
+}
+mat <- x$get()
+inv <- solve(mat, ...)
+x$setInverse(inv)
+inv
+}
+
+my_matrix <- makeCacheMatrix(matrix(1:4, 2, 2))
+my_matrix$get()
+
+my_matrix$getInverse()
+cacheSolve(my_matrix)
+cacheSolve(my_matrix)
+my_matrix$getInverse()
+my_matrix$set(matrix(c(2, 2, 1, 4), 2, 2))
+my_matrix$get()
+my_matrix$getInverse()
+cacheSolve(my_matrix)
+cacheSolve(my_matrix)
+my_matrix$getInverse()
